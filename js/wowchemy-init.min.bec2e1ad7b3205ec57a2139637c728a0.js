ddf5bb32223e2e04203ffaf8dbf03c10d0ac9866
Kannappan Jayakodi Nitthilan <kannappanjayakodinitthilan@steptoe-10-222-0-112.resnet.wsu.edu>
2021-01-25 18:13:05 -0800

    Build website

---
+++ js/wowchemy-init.min.bec2e1ad7b3205ec57a2139637c728a0.js
@@ -0,0 +1,23 @@
+/*! Wowchemy v5.0.0-beta.2 | https://wowchemy.com/ */
+/*! Copyright 2016-present George Cushen (https://georgecushen.com/) */
+/*! License: https://github.com/wowchemy/wowchemy-hugo-modules/blob/master/LICENSE.md */
+
+;
+/*************************************************
+ *  Wowchemy
+ *  https://github.com/wowchemy/wowchemy-hugo-modules
+ *
+ *  Wowchemy Initialization
+ **************************************************/
+
+import {initThemeVariation} from './wowchemy-theming';
+
+import {wcDarkLightEnabled, wcIsSiteThemeDark} from '@params';
+
+window.wc = {
+  darkLightEnabled: wcDarkLightEnabled,
+  isSiteThemeDark: wcIsSiteThemeDark,
+};
+
+// Initialize theme variation and set body theme class.
+initThemeVariation();
